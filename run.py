a75e8179b8b8464e72a44d059374377b5acbbc4c
kateuj <kate.ulloajames@gmail.com>
2024-03-10 14:33:16 +0000

    update to code to fix import db bug

--- run.py
+++ run.py
@@ -2,9 +2,9 @@ import os
 from taskmanager import app
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP"),
-        port=os.environ.get("PORT"),
+        port=int(os.environ.get("PORT")),
         debug=os.environ.get("DEBUG")
     )
